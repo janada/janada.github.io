d3018a01c29fa5855a9bd7e6f5df06156d4e19e4
Peter Klein <peter@uytr.org>
2015-07-11 02:06:55 -0400

    Done for tonight

---
+++ js/lol.js
@@ -0,0 +1,15 @@
+$(function(){
+  var step = 1000/30;
+
+  // start timed loop
+  setInterval(update, step);
+  function update(){
+    // update screen
+
+    // move jane
+    $('#i0').width($('#i0').width()+2);
+
+    // move pat
+
+  }
+})
