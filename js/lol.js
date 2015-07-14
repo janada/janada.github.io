c5780d02a71fb1b92cbb9dc08219798400cca239
Peter Klein <peter@uytr.org>
2015-07-14 01:16:25 -0400

    Still much work to be done

--- js/lol.js
+++ js/lol.js
@@ -5,11 +5,16 @@ $(function(){
   setInterval(update, step);
   function update(){
     // update screen
+    var c_x = $( window ).width()/2,
+	c_y = $( window ).height()/2;
+    // move i0
+    $('#i0').width(Date.now()%1000)
+    $('#i0').height(Date.now()%1000)
+    $('#i0').offset(function(o){
+      return { top: c_y-this.width/2, left: c_x-this.height/2 }
+    });
 
-    // move jane
-    $('#i0').width($('#i0').width()+2);
-
-    // move pat
+    // move i1
 
   }
 })
